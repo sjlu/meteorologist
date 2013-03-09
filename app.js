dc37ac104631bd834fb8dcb7e006c379d73ac9e9
Steven Lu <tacticalazn@gmail.com>
2013-03-09 00:05:17 -0500

    Refactored entirely, added tests.

--- app.js
+++ app.js
@@ -1,25 +1,250 @@
 var request = require('request');
-var parseXml = require('xml2js').parseString;
+var parser = require('xml2js').parseString;
 var moment = require('moment');
 var fs = require('fs');
 var _ = require('lodash');
 var cities = require('cities');
 
-// var url = 'http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=NDFDgenMultiZipCode&Unit=e&wx=wx&Submit=Submit';
-// var url = 'http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=NDFDgenMultiZipCode&product=glanceUnit=e&maxt=maxt&mint=mint&wx=wx&Submit=Submit'
-var url = 'http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?format=24+hourly&numDays=7';
-
-var getDay = function(time)
+var NDFD = (function() 
 {
-   var substring = time.substring(0, 10);
-   var day = moment(moment(substring, "YYYY-MM-DD").format('MMDDYYYY'), 'MMDDYYYY').format('YYYYMMDD');
-   return day;
+   var Time = (function() 
+   {
+
+      function Time(time)
+      {
+         this.time = time;
+      }
+
+      Time.prototype.getDay = function()
+      {
+         return parseInt(this.time.substring(0, 4) + this.time.substring(5, 7) + this.time.substring(8, 10));
+      }
+
+      Time.prototype.getHour = function()
+      {
+         // return this.time.substring(11, 13) + this.time.substring(14, 16);
+         return parseInt(this.time.substring(11, 13));
+      }
+
+      Time.prototype.getFrame = function()
+      {
+         var hour = this.getHour();
+         return Math.floor(hour/3)*3;
+      }
+
+      return Time;
+
+   })();
+
+   function NDFD(xmlobj) 
+   {
+      this.data = xmlobj['dwml']['data'][0];
+   }
+
+   NDFD.prototype.getTimeLayout = function(key)
+   {
+      return this.getTimeLayouts()[key];
+   }
+
+   NDFD.prototype.getTimeLayouts = function()
+   {
+      if (this.timeLayouts != null)
+         return this.timeLayouts;
+
+      var timeLayouts = {};
+      var results = this.data['time-layout'];
+      for (var i in results)
+      {
+         var intervals = [];
+         for (var j = 0; j < results[i]['start-valid-time'].length; j++)
+         {
+            // create the interval time.
+            var interval = results[i]['start-valid-time'][j];
+            intervals.push(interval);
+         }
+
+         timeLayouts[results[i]['layout-key'][0]] = intervals;
+      }
+
+      this.timeLayouts = timeLayouts;
+      return timeLayouts;
+   };
+
+   NDFD.prototype.getPredictions = function()
+   {
+      if (this.predictions != null)
+         return this.predictions;
+
+      var predictions = {};
+      var results = this.data['parameters'][0]['weather'][0];
+      // console.log(JSON.stringify(results));
+
+      var timeLayout = this.getTimeLayout(results['$']['time-layout']);
+
+      for (var j in results['weather-conditions'])
+      {
+         var time = new Time(timeLayout[j]);
+         var day = time.getDay();
+         var hour = time.getHour();
+
+         if (results['weather-conditions'][j]['$'] != null)
+         {
+            predictions[day] = {
+               attributes: {
+                  prediction: results['weather-conditions'][j]['$']['weather-summary']
+               }
+            };
+         }
+         else if (results['weather-conditions'][j]['value'] != null)
+         {
+            var value = results['weather-conditions'][j]['value'];
+            var string = '';
+            for (var k in value)
+            {
+               var condition = value[k]['$'];
+               string = condition.coverage + ' of ' + condition.intensity + ' ' + condition['weather-type'];
+               if (condition.qualifier != 'none') string = string + ' ' + qualifier + ' ';
+            }
+
+            if (predictions[day] == null)
+               predictions[day] = {};
+
+            if (predictions[day]['hours'] == null)
+               predictions[day]['hours'] = {};
+
+            predictions[day]['hours'][hour] = string;
+         }
+         else
+         {
+            if (predictions[day] == null)
+               predictions[day] = {};
+
+            if (predictions[day]['hours'] == null)
+               predictions[day]['hours'] = {};
+
+            predictions[day]['hours'][hour] = 'clear';
+         }
+      }
+
+      return predictions;
+   }
+
+   NDFD.prototype.getTemps = function()
+   {
+      if (this.temps != null)
+         return this.temps;
+
+      var temps = {};
+      var results = this.data['parameters'][0]['temperature'];
+      var types = [];
+
+      for (var i in results)
+      {
+         // gotta figure out what time layout we're using.
+         var timeLayout = this.getTimeLayout(results[i]['$']['time-layout']);
+         
+         var type = results[i]['$'].type;
+         if (type == 'hourly')
+            type = 'actual';
+
+         types.push(type);
+
+         // so each value is directly correlated to our
+         // values of time layouts. we'll walk through each
+         // and see the temperature for the day.
+         for (var j in results[i].value)
+         {
+            if (typeof results[i].value[j] === "object")
+               continue;
+
+            var time = new Time(timeLayout[j]);
+            var day = time.getDay();
+            var hour = time.getHour();
+
+            if (temps[day] == null)
+               temps[day] = {};
+
+            if (temps[day]['hours'] == null)
+               temps[day]['hours'] = {};
+
+            if (temps[day]['hours'][hour] == null)
+               temps[day]['hours'][hour] = {}
+
+            temps[day]['hours'][hour][type] = results[i].value[j];
+         }
+      }
+
+      for (var k in temps)
+      {
+         if (temps[k]['attributes'] == null)
+            temps[k]['attributes'] = {
+               temperatures: {
+                  min: {},
+                  max: {}
+               }
+            };
+
+         for (var z in types)
+         {
+            temps[k]['attributes']['temperatures']['min'][types[z]] = _.min(temps[k]['hours'], function(a) { return a[types[z]]; })[types[z]];
+            temps[k]['attributes']['temperatures']['max'][types[z]] = _.max(temps[k]['hours'], function(a) { return a[types[z]]; })[types[z]];
+         }
+      }
+
+      this.temps = temps;
+      return temps;
+   };
+
+   return NDFD;
+
+})();
+
+var urls = { 
+   summary: 'http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?format=24+hourly&numDays=7',
+   hourly: 'http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=NDFDgenMultiZipCode&product=time-series&Unit=e&temp=temp&qpf=qpf&pop12=pop12&snow=snow&wspd=wspd&sky=sky&wx=wx&rh=rh&appt=appt&Submit=Submit'
 };
 
-var getHour = function(time)
+exports.retrieve = function(url, type, evtHandler)
 {
-   var hour = '' + time.substring(11, 13) + time.substring(14, 16);
-   return hour;
+   request(url, function (error, response, body) 
+   {
+      if (error || response.statusCode != 200) 
+         return evtHandler({error:'Invalid response.'});
+
+      parser(body, function (err, result)
+      {
+         if (err || typeof result['dwml']['data'][0] === undefined || result.error)
+            return evtHandler({error: 'Unrecognized format.'});
+
+         return evtHandler(new NDFD(result), type);
+      });
+   });
+};
+
+exports.execute = function(zipcode, evtHandler)
+{
+   var location = cities.zip_lookup(zipcode);
+   if (!location)
+      return evtHandler({error: 'Location invalid.'});
+
+   var results = {};
+
+   var responseHandler = function(result, type)
+   {
+      if (result.error)
+         return evtHandler(result);
+
+      results[type] = result;
+
+      if (_.keys(urls).length != _.keys(results).length)
+         return;
+      
+      results.location = location;
+      return evtHandler(results);
+   };
+
+   for (var type in urls)
+      exports.retrieve(urls[type] + '&zipCodeList=' + zipcode, type, responseHandler);
 };
 
 exports.forecast = function(zipcode, evtHandler)
@@ -27,133 +252,38 @@ exports.forecast = function(zipcode, evtHandler)
    if (typeof evtHandler != 'function')
       return;
 
-   var location = cities.zip_lookup(zipcode);
-   if (!location)
-      return evtHandler({error: 'Location invalid.'});
-
-   request(url + '&zipCodeList=' + zipcode, function (error, response, body) 
+   var responseHandler = function(results)
    {
-      if (error || response.statusCode != 200) 
-         return evtHandler({error:'Invalid response.'});
+      if (results.error)
+         return evtHandler(results);
 
-      parseXml(body, function (err, result)
+      var output = {
+         location: results.location,
+         weather: []
+      };
+
+      // var data = {
+      //    forecast: results.summary.getPredictions(),
+      //    temperatures: results.hourly.getTemps()
+      // };
+
+      var data = _.merge(results.summary.getPredictions(), results.hourly.getTemps());
+
+      for (var day in data)
       {
-         if (err || typeof result['dwml']['data'][0] === undefined)
-            return evtHandler({error: 'Unrecognized format.'});
+         output.weather.push({
+            day: {
+               numeric: day,
+               readable: moment(day, 'YYYYMMDD').format('dddd, MMMM D, YYYY')
+            },
+            forecast: data[day].attributes
+         });
+      }
 
-         // just ignore some stuff we don't need.
-         result = result['dwml']['data'][0];
-         var results = {
-            times: result['time-layout'],
-            temps: result['parameters'][0]['temperature'],
-            predictions: result['parameters'][0]['weather'][0]
-         };
-         var forecast = {};
+      evtHandler(output);
+   };
 
-         // we need to parse the time frames out,
-         // so that way we can use it parse 
-         // other data out
-         var times = {};
-         for (var i in results.times)
-         {
-            var intervals = [];
-            for (var j = 0; j < results.times[i]['start-valid-time'].length; j++)
-            {
-               // create the interval time.
-               var interval = results.times[i]['start-valid-time'][j];
+   exports.execute(zipcode, responseHandler);
+};
 
-               // we need to start-up our response with days
-               // as that's how we're going to respond to people
-               var day = getDay(interval);
-               if (typeof forecast[day] === "undefined")
-                  forecast[day] = {};
-
-               intervals.push(interval);
-            }
-
-            times[results.times[i]['layout-key'][0]] = intervals;
-         }
-
-         // building out temperature to days.
-         // this for loop is for looking at min and max
-         // will only ever run twice.
-         for (var i in results.temps)
-         {
-            // gotta figure out waht itme layout we're using.
-            var timeLayout = results.temps[i]['$']['time-layout'];
-            
-            // high and low makes more sense.
-            var type = results.temps[i]['$'].type;
-            if (type == 'maximum')
-               type = 'high';
-            else
-               type = 'low';
-
-            // so each value is directly correlated to our
-            // values of time layouts. we'll walk through each
-            // and see the temperature for the day.
-            for (var j in results.temps[i].value)
-            {
-               if (typeof results.temps[i].value[j] === "object")
-                  continue;
-
-               var day = getDay(times[timeLayout][j]);
-
-               // build it out into our array.
-               if (typeof forecast[day].temperatures === "undefined")
-                  forecast[day].temperatures = {};
-
-               forecast[day].temperatures[type] = results.temps[i].value[j];
-            }
-         }
-
-         // building out hour to hour forecasts to days.
-         // it is always one to one, and only one result set.
-         var timeLayout = results.predictions['$']['time-layout'];
-         results.predictions = results.predictions['weather-conditions'];
-         // looking at each prediction
-         for (var i in results.predictions)
-         {
-            // if (typeof results.predictions[i]['value'] === "undefined")
-            //    continue;
-
-            // each interval is always on the hour
-            // we just need to get the day.
-            var hour = getHour(times[timeLayout][i]);
-            var day = getDay(times[timeLayout][i]); 
-
-            // take the prediction and place it directly into the object.
-            forecast[day].prediction = results.predictions[i]['$']['weather-summary'];
-         }
-
-         // console.log(JSON.stringify(forecast));
-
-         var formatted = [];
-         for (var i in forecast)
-         {
-            if (typeof forecast[i].prediction === "undefined")
-               continue;
-
-            var format = {};
-            var day = moment(i, 'YYYYMMDD');
-
-            // format day
-            format.day = {
-               "numeric": i,
-               "readable": day.format('dddd, MMMM D, YYYY')
-            }
-
-            // format temps
-            format.temperatures = forecast[i].temperatures;
-            format.prediction = forecast[i].prediction;
-
-            formatted.push(format);
-         }
-
-         // console.log(JSON.stringify(formatted));
-
-         if (typeof evtHandler === 'function')
-            return evtHandler({location: location, weather: formatted});
-      });
-   });
-}
+// exports.forecast('07946', function(e) { console.log(JSON.stringify(e)); });
---
+++ test/app.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var m = require("../app");
+
+describe('forecast', function()
+{
+   it('the forecast object should be fully populated', function(done)
+   {
+      m.forecast('07946', function(e){
+         assert.equal(typeof e.location, 'object');
+         assert.equal('07946', e.location.zipcode);
+         assert.equal(typeof e.weather, 'object');
+         assert.equal(typeof e.weather[0], 'object');
+         assert.equal(typeof e.weather[0].day, 'object');
+         assert.equal(typeof e.weather[0].forecast, 'object');
+         assert.equal(typeof e.weather[0].forecast.prediction, 'string');
+         assert.equal(typeof e.weather[0].forecast.temperatures, 'object');
+         done();
+      });
+   });
+});
